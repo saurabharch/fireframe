7215dfbdf022a91b9fd8891ec7fb583c1890b0ed
Liana <lrc47@cornell.edu>
2016-03-16 19:48:45 -0400

    some routes, some changes to some models

---
+++ server/app/routes/projects/index.js
@@ -0,0 +1,8 @@
+/* projects route */
+
+'use strict';
+var router = require('express').Router();
+module.exports = router;
+
+var mongoose = require('mongoose');
+var Project = mongoose.model('Project');
