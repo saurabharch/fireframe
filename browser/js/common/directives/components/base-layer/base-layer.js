34246f5702e2483e294d3f7c5f7bbdbf0ea1bb8d
jeremybini <jeremybini@gmail.com>
2016-03-16 19:11:07 -0400

    draggin

---
+++ browser/js/common/directives/components/base-layer/base-layer.js
@@ -0,0 +1,12 @@
+app.directive('baseLayer', function() {
+	return {
+		restrict: 'E',
+		templateUrl: '/js/common/directives/components/base-layer/base-layer.html'
+		// scope: {
+
+		// },
+		// link: function(scope, element, attr) {
+
+		// }
+	}
+});
