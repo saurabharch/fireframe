1c946861fa83b11435056925b908b6896afb89f8
Gustaf Erickson <gerickson808@gmail.com>
2016-03-17 16:45:54 -0400

    Chaaaanges

--- browser/js/editor/editor.js
+++ browser/js/editor/editor.js
@@ -1,7 +1,34 @@
 app.config(function($stateProvider){
 	$stateProvider.state('editor', {
 		url: '/editor',
-		templateUrl: '/js/editor/editor.html'
+		templateUrl: '/js/editor/editor.html',
+		resolve: {
+			wireframe: function() {
+				return { components: [], master: true };
+			}
+		},
+		controller: 'EditorCtrl'
 		});
-		// controller: 'EditorCtrl'
+});
+
+app.controller('EditorCtrl', function($scope, wireframe, $compile, Component, Interact) {
+	$scope.wireframe = wireframe;
+	$scope.board = $('#wireframe-board');
+	$scope.active = null;
+	$scope.elementsRendered = $scope.elementsRendered || false;
+
+	if(!$scope.elementsRendered) {
+		Component.load($scope.wireframe.components, $scope);
+	}
+
+	Interact.dragAndResize();
+
+	$scope.loadElements = function() {
+		Component.load(wireframe.components, $scope);
+	};
+
+	$scope.createElement = function(type) {
+		Component.create(type, $scope);
+	};
+
 });
