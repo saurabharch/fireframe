86831ac1c6b49705d5160704d93389aa6aac0cde
Gustaf Erickson <gerickson808@gmail.com>
2016-03-23 16:08:28 -0400

    Shrimping

---
+++ browser/js/signup/signup.js
@@ -0,0 +1,32 @@
+app.config(function ($stateProvider) {
+
+    $stateProvider.state('signup', {
+        url: '/signup',
+        templateUrl: 'js/signup/signup.html',
+        controller: 'SignupCtrl'
+    });
+
+});
+
+app.controller('SignupCtrl', function ($scope, AuthService, $state) {
+
+    $scope.signup = {};
+    $scope.error = null;
+
+    $scope.sendSignup = function (signupInfo) {
+        if($scope.passwordConfirm !== $scope.signup.password){
+            $scope.error = "Password confirmation does not match!";
+            return;
+        }
+        $scope.error = null;
+
+
+        AuthService.signup(signupInfo).then(function () {
+            $state.go('home');
+        }).catch(function () {
+            $scope.error = 'Invalid signup credentials.';
+        });
+
+    };
+
+});
