351b73f49b4be327809ca74a614dba6c3ba70644
Gustaf Erickson <gerickson808@gmail.com>
2016-03-23 11:47:42 -0400

    Add Project Broheim

--- browser/js/common/factories/User.js
+++ browser/js/common/factories/User.js
@@ -1,16 +1,24 @@
-app.factory('UserFactory', function($http){
+app.factory('UserFactory', function($http, $log){
 
 	var factory = {
 
 		getUserTeams: function(userId){
 			return $http.get('/api/user/'+userId+'/teams')
-			.then(response => response.data);
+			.then(response => response.data)
+			.catch($log);
 		},
 
 		addTeam: function(team){
 			return $http.post('/api/teams', team)
-			.then(response => response.data);
-		}
+			.then(response => response.data)
+			.catch($log);
+		},
+
+		addProject: function(project){
+			return $http.post('/api/projects', project)
+			.then(response => response.data)
+			.catch($log);
+		}		
 
 	};
 
