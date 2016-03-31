48e906d991968f864056d715239d353e31015e10
jeremybini <jeremybini@gmail.com>
2016-03-31 17:43:12 -0400

    started dialog box for new project

--- browser/js/dashboard/dashboard.js
+++ browser/js/dashboard/dashboard.js
@@ -8,7 +8,7 @@ app.config(function($stateProvider) {
 	})
 });
 
-app.controller('DashboardCtrl', function($scope, $state, $mdSidenav) {
+app.controller('DashboardCtrl', function($scope, $state, $mdSidenav, $mdDialog) {
 
 	$state.go('dashboard.allProjects');
 
@@ -16,6 +16,21 @@ app.controller('DashboardCtrl', function($scope, $state, $mdSidenav) {
 		if($(this).find('a').hasClass('add')) return;
 		$('.sidebar li').removeClass('active');
 		$(this).addClass('active');
+		$mdSidenav('left').toggle();
 	});
 
+	$scope.showAlert = function(ev) {
+    $mdSidenav('left').toggle();
+    // Appending dialog to document.body to cover sidenav in docs app
+    // Modal dialogs should fully cover application
+    // to prevent interaction outside of dialog
+    $mdDialog.show({
+    	templateUrl: '/js/dashboard/dashboard.new-project.html',
+      $mdDialog.alert()
+        .parent(angular.element(document.querySelector('#popupContainer')))
+        .clickOutsideToClose(true)
+        .targetEvent(ev)
+    }
+    );
+  };
 });
