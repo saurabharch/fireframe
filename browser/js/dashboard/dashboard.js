85aaa7af4df7ee0c49e668438142de1abc3d909f
Gustaf Erickson <gerickson808@gmail.com>
2016-04-01 15:09:36 -0400

    Exorcised the usurper, Angular Material

--- browser/js/dashboard/dashboard.js
+++ browser/js/dashboard/dashboard.js
@@ -8,25 +8,12 @@ app.config(function($stateProvider) {
 	})
 });
 
-app.controller('DashboardCtrl', function($scope, $state, $mdSidenav, $mdDialog) {
+app.controller('DashboardCtrl', function($scope, $state) {
 
 	$('.sidebar li').click(function() {
 		if($(this).find('a').hasClass('add')) return;
 		$('.sidebar li').removeClass('active');
 		$(this).addClass('active');
-		$mdSidenav('left').toggle();
 	});
 
-	$scope.showAlert = function(event) {
-    $mdSidenav('left').toggle();
-    // Appending dialog to document.body to cover sidenav in docs app
-    // Modal dialogs should fully cover application
-    // to prevent interaction outside of dialog
-		var options = $mdDialog.show({
-			parent: angular.element(document.querySelector('body')),
-			templateUrl: '/js/dashboard/dashboard.new-project.html',
-			targetEvent: event,
-			clickOutsideToClose: true
-		})
-  };
 });
